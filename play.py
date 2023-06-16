c80ac9f501ecb54115f5c395856fbe30e9266431
Mazi11i11 <63658632+Mazi11i11@users.noreply.github.com>
2023-06-15 21:45:26 -0400

    Update play.py

--- play.py
+++ play.py
@@ -1,19 +1,26 @@
 import random
 
-def simulate_at_bat(batter_avg):
-    # Simulate a single at-bat and return the outcome
-    if random.random() < batter_avg:
+class Player:
+    def __init__(self, name, batting_avg):
+        self.name = name
+        self.batting_avg = batting_avg
+
+def simulate_at_bat(player):
+    # Simulate a single at-bat for a specific player and return the outcome
+    if random.random() < player.batting_avg:
         return "Hit"
     else:
         return "Out"
 
-def simulate_inning(batter_avg):
+def simulate_inning(team):
     outs = 0
     runs = 0
 
     # Simulate at-bats until three outs occur
     while outs < 3:
-        outcome = simulate_at_bat(batter_avg)
+        # Select a player randomly from the team's roster
+        batter = random.choice(team)
+        outcome = simulate_at_bat(batter)
         if outcome == "Hit":
             runs += 1
         else:
@@ -21,18 +28,26 @@ def simulate_inning(batter_avg):
 
     return runs
 
-def simulate_game(team1_avg, team2_avg, innings):
+def simulate_game(team1, team2, innings):
     team1_score = 0
     team2_score = 0
 
     # Simulate innings and update scores
     for _ in range(innings):
-        team1_score += simulate_inning(team1_avg)
-        team2_score += simulate_inning(team2_avg)
+        team1_score += simulate_inning(team1)
+        team2_score += simulate_inning(team2)
 
     # Display final scores
     print("Team 1:", team1_score)
     print("Team 2:", team2_score)
 
 # Example usage
-simulate_game(0.300, 0.275, 9)
+player1 = Player("Player 1", 0.300)
+player2 = Player("Player 2", 0.275)
+team1 = [player1, player2]
+
+player3 = Player("Player 3", 0.320)
+player4 = Player("Player 4", 0.290)
+team2 = [player3, player4]
+
+simulate_game(team1, team2, 9)
